4216e5db6be2e39432a49f214ad005ca66871a70
Robin Nowak <44155928+staxie@users.noreply.github.com>
2021-02-02 15:47:04 +0100

    Refactored Switch statement

--- StrategyPattern/src/Main.java
+++ StrategyPattern/src/Main.java
@@ -5,9 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		Route route = new Route();
 
-		System.out.println("Bitte wählen Sie ein Verkehrsmittel zur Routenberechnung aus:");
+		System.out.println("Bitte wÃ¤hlen Sie ein Verkehrsmittel zur Routenberechnung aus:");
 		System.out.println("1 = Auto");
-		System.out.println("2 = Öffentliche Verkehrsmittel");
+		System.out.println("2 = Ã–ffentliche Verkehrsmittel");
 		System.out.println("3 = Fahrrad");
 		
 		
@@ -23,21 +23,20 @@ public class Main {
 		
 		switch (userInputNumber) {
 		case 0:
-			System.out.println("Keine gültige Auswahl. Bitte wählen Sie eine Zahl von 1-3 aus.");
+			System.out.println("Keine gÃ¼ltige Auswahl. Bitte wÃ¤hlen Sie eine Zahl von 1-3 aus.");
 			break;
 		case 1:
 			route.setRouting(new CarRouting());
-			route.createRoute();
 			break;
 		case 2: 
 			route.setRouting(new PublicTransportRouting());
-			route.createRoute();
 			break;
 		case 3:
 			route.setRouting(new BikeRouting());
-			route.createRoute();
 			break;
 		}
+		
+		route.createRoute();
 
 	}
 
